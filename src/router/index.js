c4d9808b67d8c18ac7a4fc892db5f73cfbdc8e8c
eldadYikne <nmknv99@gmail.com>
2022-11-01 00:22:56 +0200

    createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../components/home-page.vue'
 import ContactList from '../components/contact-list.vue'
 import ContactDetails from '../components/contact-details.vue'
@@ -7,20 +7,18 @@ import Signup from '../components/signup.vue'
 import Transfer from '../components/transfer-contact.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       component: HomePage,
       meta: { transition: 'fade' },
-      mode : 'hash' 
 
     },
     {
       path: '/contact',
       component: ContactList,
       meta: { transition: 'fade' },
-      mode : 'hash'
       
 
     },
@@ -28,28 +26,24 @@ const router = createRouter({
       path: '/transfer/:contactId',
       component: Transfer,
       meta: { transition: 'fade' },
-      mode : 'hash'
 
     },
     {
       path: '/signup',
       component: Signup,
       meta: { transition: 'fade' },
-      mode : 'hash'
 
     },
     {
       path: '/contact/:contactId',
       component: ContactDetails,
       meta: { transition: 'fade' },
-      mode : 'hash'
 
     },
     {
       path: '/contact/edit/:editId?',
       component: ContactEdit,
       meta: { transition: 'fade' },
-      mode : 'hash'
     },
 
   ]
